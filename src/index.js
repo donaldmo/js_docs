1a6eadf3b804d2ddb6189f39e277528a05ce2c0f
Donald Motswiri <domotswiri@gmail.com>
2024-04-08 07:45:02 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+/**
+ * Student Name
+ * @type {string}
+ */
+const studentName = "John Doe";
+
+/**
+ * Array of grades
+ * @type {Array<number>}
+ */
+const grades = [1, 12, 3, 4, 5]
+
+/**
+ * Object todo
+ * @type {{id: number|string, text: string}}
+ */
+const todo = {
+    id: 1,
+    text: "Hello"
+}
